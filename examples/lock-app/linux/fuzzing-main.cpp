d956ad12bcea17bb909ac7b103325ebe763914ed
Qingyun Wu <ikelvinwoo@gmail.com>
2023-09-23 20:05:31 +1000

    make lock-app fuzzable

---
+++ examples/lock-app/linux/fuzzing-main.cpp
@@ -0,0 +1,142 @@
+/*
+ *    Copyright (c) 2022 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "AppMain.h"
+#include "LockAppCommandDelegate.h"
+#include "LockManager.h"
+#include <app/server/Server.h>
+#include <transport/SessionManager.h>
+#include <transport/TransportMgr.h>
+#include <transport/raw/PeerAddress.h>
+
+#include <CommissionableInit.h>
+
+#include <chrono>
+#include <iostream>
+
+extern "C" void __gcov_dump();
+
+using namespace chip;
+using namespace chip::DeviceLayer;
+
+namespace {
+
+LinuxCommissionableDataProvider gCommissionableDataProvider;
+
+}
+
+Transport::PeerAddress AddressFromString(const char * str)
+{
+    Inet::IPAddress addr;
+
+    VerifyOrDie(Inet::IPAddress::FromString(str, addr));
+
+    return Transport::PeerAddress::UDP(addr);
+}
+
+uint16_t kLocalSessionId = 1;
+uint16_t kPeerSessionId = 2;
+const NodeId kLocalNodeId = 123;
+const NodeId kPeerNodeId = 123;
+const FabricIndex kFabricIndex = 1;
+const Transport::PeerAddress kPeerAddress = AddressFromString("fe80::1");
+
+void CleanShutdown()
+{
+    Server::GetInstance().Shutdown();
+    PlatformMgr().Shutdown();
+    // TODO: We don't Platform::MemoryShutdown because ~CASESessionManager calls
+    // Dnssd::ResolverProxy::Shutdown, which starts doing Platform::Delete.
+    // Platform::MemoryShutdown();
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t * aData, size_t aSize)
+{
+    static auto fuzzCampaignStart = std::chrono::steady_clock::now();
+    static auto fuzzCampaignMinutes = [](){
+        char *envString = getenv("FUZZ_CAMPAIGN_MINUTES");
+
+        int minutes = (envString == NULL) ? 0 : atoi(envString);
+        if (minutes > 0) std::cerr << "FUZZ_CAMPAIGN_MINUTES: " << minutes << std::endl;
+
+        return minutes;
+    } ();
+
+    // Check elapsed time
+    if (fuzzCampaignMinutes > 0) {
+        auto current = std::chrono::steady_clock::now();
+        auto elapsedMinutes = std::chrono::duration_cast<std::chrono::minutes>(current - fuzzCampaignStart).count();
+        if (elapsedMinutes >= fuzzCampaignMinutes) {
+            // Passed scheduled end
+            std::cerr << "Stopping fuzzing after " << elapsedMinutes << " minutes" << std::endl; 
+            __gcov_dump();
+            exit(0);
+        }
+    }
+
+    static bool matterStackInitialized = false;
+    if (!matterStackInitialized)
+    {
+        VerifyOrDie(Platform::MemoryInit() == CHIP_NO_ERROR);
+        VerifyOrDie(PlatformMgr().InitChipStack() == CHIP_NO_ERROR);
+
+	    VerifyOrDie(chip::examples::InitCommissionableDataProvider(gCommissionableDataProvider,
+                                                                   LinuxDeviceOptions::GetInstance()) == CHIP_NO_ERROR);
+        SetCommissionableDataProvider(&gCommissionableDataProvider);
+
+
+        // ChipLinuxAppMainLoop blocks, and we don't want that here.
+        static chip::CommonCaseDeviceServerInitParams initParams;
+        (void) initParams.InitializeStaticResourcesBeforeServerInit();
+        VerifyOrDie(Server::GetInstance().Init(initParams) == CHIP_NO_ERROR);
+
+        // We don't start the event loop task, because we don't plan to deliver
+        // data on a separate thread.
+
+        matterStackInitialized = true;
+
+        // And add a test session
+        SessionHolder testSessionHolder;
+        Server::GetInstance().GetSecureSessionManager().InjectCaseSessionWithTestKey(testSessionHolder, kLocalSessionId,
+            kPeerSessionId, kLocalNodeId, kPeerNodeId, kFabricIndex, kPeerAddress, CryptoContext::SessionRole::kResponder);
+
+        // The fuzzer does not have a way to tell us when it's done, so just
+        // shut down things on exit.
+        atexit(CleanShutdown);
+    }
+
+    // For now, just dump the data as a UDP payload into the session manager.
+    // But maybe we should try to separately extract a PeerAddress and data from
+    // the incoming data?
+    Transport::PeerAddress peerAddr;
+    System::PacketBufferHandle buf =
+        System::PacketBufferHandle::NewWithData(aData, aSize, /* aAdditionalSize = */ 0, /* aReservedSize = */ 0);
+    if (buf.IsNull())
+    {
+        // Too big; we couldn't represent this as a packetbuffer to start with.
+        return 0;
+    }
+
+    // Ignoring the return value from OnMessageReceived, because we might be
+    // passing it all sorts of garbage that will cause it to fail.
+    Server::GetInstance().GetSecureSessionManager().OnMessageReceived(peerAddr, std::move(buf));
+
+    // Now process pending events until our sentinel is reached.
+    PlatformMgr().ScheduleWork([](intptr_t) { PlatformMgr().StopEventLoopTask(); });
+    PlatformMgr().RunEventLoop();
+    return 0;
+}
